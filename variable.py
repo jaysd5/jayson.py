742aa45280515b1f98085d80423331d9be3e091e
jayson <decubberjayson@gmail.com>
2022-11-29 11:53:28 +0100

    j'ai rajouter une variable avec des nombre

--- variable.py
+++ variable.py
@@ -1,3 +1,13 @@
+# l'operateur  d'affection = permet d'injecter une valeur dans une variable 
+
+
+my_number1 = 123
+my_number2 = 152
+
+
+print(my_number1)
+print(my_number2)
+
 my_text1 = "ceci est une nouvelle chaine de caractères"
 my_text2 = "ceci est une nouvelle chaine de caractères"
 
