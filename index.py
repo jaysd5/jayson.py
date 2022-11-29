1a7b291a3c89fe9cc742d991027ad3a087e8e032
jayson <decubberjayson@gmail.com>
2022-11-29 15:57:43 +0100

    memo de variable

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 #!/bin/python
 
 print("hello pyton!")
+
+
+loisir = ["gaming", "musique", "clavier", "souris"]
+print(loisir[0])
+print(loisir[1])
+print(loisir[2])
+print(loisir[3])
+
+for loisir in loisir :
+    print(loisir)
+    print(loisir*4)
+print("bravo tu c'est faire un variable")
