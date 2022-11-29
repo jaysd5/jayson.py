d9d185a6b1bd91f2499450cdb6b9924e426347d0
jayson <decubberjayson@gmail.com>
2022-11-29 14:43:10 +0100

    une variable drole

--- variable.py
+++ variable.py
@@ -13,3 +13,14 @@ my_text2 = "ceci est une nouvelle chaine de caractères"
 
 print(my_text1)
 print(my_text2)
+
+loisir = ["gaming", "musique", "clavier", "souris"]
+print(loisir[0])
+print(loisir[1])
+print(loisir[2])
+print(loisir[3])
+
+for loisir in loisir :
+    print(loisir)
+    print(loisir*4)
+print("bravo tu c'est faire un variable")
