a187d6948a1e6b3c025cf577dc2f0c6dae68c474
jayson <decubberjayson@gmail.com>
2022-12-05 11:25:32 +0100

    variable de python

--- index.py
+++ index.py
@@ -1,15 +1,47 @@
 #!/bin/python
-
+# int nombre entier 
 my_number1 = 123
-my_number2 = 42 
+my_number2 = 42
+my_number6 = 0 
 
 print (my_number1)
 print (my_number2)
+print (my_number6)
+print (type(my_number1))
 
+# float nombre a virgule flottante 
 my_number3 = 3.14
 my_number4 = 2.71
-my_number5 = 0.0 
+# 0.0 ou 0. ou .0 
+my_number5 = 0.0
 
 print (my_number3)
 print (my_number4)
 print ( my_number5)
+print (type(my_number3))
+
+# bool , booléen 
+my_boolean1 = True 
+my_boolean2 = False
+print (my_boolean1)
+print (my_boolean2)
+print (type(my_boolean1))
+
+# None valeur nulle 
+# null , nil
+my_none = None 
+print (my_none)
+print (type(my_none))
+
+# sting , chaine de caractéres
+# double quote ou simple quote c'est pareill
+my_texte1 = "ceci est une chaine de caractére"
+my_texte2 = "ceci est une chaine de caractére"
+
+print  (my_texte1)
+print  (my_texte2)
+
+# \ caratére d'echapement 
+# \ n saut a la ligne 
+my_texte3 = "abc\def\ghi"
+print (my_texte3)
