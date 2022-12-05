fa80647c07afc2291d0aafabbd873df70f4d1765
jayson <decubberjayson@gmail.com>
2022-12-05 10:31:35 +0100

    memo variable

--- variable.py
+++ variable.py
@@ -19,11 +19,25 @@ loisir = ["gaming", "musique", "clavier", "souris"]
 for i, loisir in enumerate (loisir):
      print(f"loisir {i} est un(e) {loisir}")
 
+print(loisir)
+print(loisir)
+print(loisir)
+print(loisir)
+
+print("bravo tu c'est faire un variable")
+
+
+print("hello pyton!")
+
+
+loisir = ["gaming", "musique", "clavier", "souris"]
 print(loisir[0])
 print(loisir[1])
 print(loisir[2])
 print(loisir[3])
 
-print("bravo tu c'est faire un variable")
-
+for loisir in loisir :
+    print(loisir)
+    print(loisir*1)
+print("bravo tu sais faire une variable")
 
